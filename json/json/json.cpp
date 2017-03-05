b1cbfd1e25b44686a1b39a6fa9d13f1dfe6ca8a0
Sam Hardy <sjh91@zips.uakron.edu>
2017-03-05 10:55:20 -0500

    parse

--- json/json/json.cpp
+++ json/json/json.cpp
@@ -1,33 +1,94 @@
 // json.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
-#include "json.h"
-#include <stddef.h>
+//#include "stdafx.h"
+//#include "json.h"
+#include <cctype>
+#include <string>
+#include <fstream>
 #include <iostream>
+#include <vector>
+#include <memory>
+#include <map>
+struct Value
+{
+	virtual ~Value() = default;
+	virtual void parse(std::string::iterator * & First, std::vector<std::unique_ptr<Value>> &v, int & weight);
+};
+
+struct Null :Value
+{
+	Null() = default;
+
+	virtual void parse(std::string::iterator  * & First, std::vector<std::unique_ptr<Value>> &v, int & weight)
+	{
+		char temp = v.size() + 1;
+		v.resize(temp);
+		v[temp].reset(new Null());
+		First + 4; //null
+		++weight; // 1 to weight for null
+	}
+
+};
+
+struct Bool :Value
+{
+	bool Val;
+	Bool() = default;
+	Bool(bool B) : Val(B) {}
+	
+	virtual void parse(std::string::iterator  & First, std::vector<std::unique_ptr<Value>> &v, int & weight)
+	{
+		if (* First == 'T') 
+		{
+			char temp = v.size() + 1;
+			v.resize(temp);
+			v[temp].reset(new Bool(true));
+			First + 4;
+			++weight;
+		}
+	}
+};
+
+struct Number :Value
+{
+	double Num;
+	Number() = default;
+	Number(double N) : Num(N) {}
+
+	virtual void parse(std::string::iterator  & First, std::vector<std::unique_ptr<Value>> &v, int & weight)
+	{
+		char number;
+		while (isdigit(*First))
+		{
 
 
-Value * Parce(char * & First, char * & Last) {
-	while(First != Last && std::isspace(*First))
+		}
+	}
+};
+
+struct String :Value, std::string
+{
+	using std::string::string;
+};
+
+struct array :Value //std::vector<Value *>
+{
+	std::vector <Value*> Value;
+
+	//~array()
+	//{
+	//for (Value:: this*)
+	//{
+	//delete this;
+	//}
+
+};
+Value * Parce(char * & First, char * Last)
+{
+	while (First != Last && std::isspace(*First))
 		++First;
-	if (First == "T" || First == "t")
-		Parce_true(First, Last);
-
-	if (First == "F" || First == "f")
-		Parce_false(First, Last);
-
-	if (First == nullptr)
-		Parce_false(First, Last);
-
-	if (First == "[")
-		Parce_array(First, Last);
-
-	//if (First == """)
-		//Parce_string(First, Last);
-	if (First == "1"|| First == "2" || First == "3" || First == "4" || First == "5" || First == "6"|| First == "7" || First == "8" || First == "9");
-		Parce_number(First, Last);
 }
-//Value * Parce_true(char * & f, char * &);
 int main()
 {
 	
